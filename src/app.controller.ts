8814c11af447401fa3e1308882f89e64b78269b2
Stephen Castaneda <stephencastaneda@Stephens-MBP.lan>
2023-05-23 13:27:59 -0700

    added login logic and mock user data retreival function

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,34 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+
+@Post('/login')
+login(@Body() { email, password }): any {
+  // Mock user data
+  const users = [
+    { id: 1, email: 'test@example.com', password: 'password123' },
+    { id: 2, email: 'user@example.com', password: 'test123' },
+  ];
+
+  const user = users.find((user) => user.email === email && user.password === password);
+
+  if (user) {
+    // Mock user data to return upon successful login
+    const userData = {
+      id: user.id,
+      email: user.email,
+      name: 'John Doe',
+    };
+
+    return { success: true, data: userData };
+  } else {
+    return { success: false, message: 'Invalid email or password' };
+  }
+}
+  @Get('/user')
+  getUser(): any {
+    return this.appService.getUserData();
   }
 }
